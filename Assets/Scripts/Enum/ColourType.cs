e2b3032dc9528cb7f0ff5cea148240089f326207
valentecaio <cvalente.aio@gmail.com>
2023-04-25 21:32:02 -0300

    add Link colour

--- Assets/Scripts/Enum/ColourType.cs
+++ Assets/Scripts/Enum/ColourType.cs
@@ -1,5 +1,5 @@
 public enum ColourType
 {
     _Default, Black, Blue, Brown, Dark_blue, Dark_purple, Golden, Green, Grey,
-    Light_blue, Light_green, Orange, Pink, Purple, Red, White, Yellow,
+    Light_blue, Light_green, Orange, Pink, Purple, Red, White, Yellow, Link,
 }
